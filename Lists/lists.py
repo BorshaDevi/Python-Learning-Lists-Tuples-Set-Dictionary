6b026ebba1cf4b139d11099425d0d51627765871
Borsha Devi <borshadevi27@gmail.com>
2025-12-20 09:56:18 +0600

    Now I learned about list some properties like,list len , list type,in list store different types of elements

---
+++ Lists/lists.py
@@ -0,0 +1,18 @@
+# 1. A built-in data type that stores a set of values.
+list=[]
+print(type(list))
+# Class 'List'
+print (len(list))
+# 5
+
+# Lists some property similar to string property.
+# In string, we can go with a particular index and find the character, similar thinks we can do in List.
+list1=[34,4,35,23,45,64,84,92]
+print(list1[0])
+# 34 
+
+
+# 2. It can store elements of different types (integer,float, string,etc.).
+# It means we can store multiple  data types of value in a single List.
+student=['karan',34 ,48.2 , 'Bangladesh']
+print(student)
