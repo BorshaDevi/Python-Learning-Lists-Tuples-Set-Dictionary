0d0ff7250ceafee04212032a6601e0e973f72be6
Borsha Devi <borshadevi27@gmail.com>
2025-12-21 10:17:51 +0600

    Now I learned about lists append and sort function.with append,add value of the end in the list.with sort,arranging values in the correct order

--- Lists/lists.py
+++ Lists/lists.py
@@ -59,3 +59,23 @@
 # List method
 
 # append(): It added value to the end of the list.
+list=[1,2,3,4,5]
+list.append(6)
+print(list)
+# [1, 2, 3, 4, 5, 6]    There is an add element that is called muted .
+
+# sort():sort means arranging values in the correct order.
+# 1. Ascending.[ascending means low to high / small to big]
+# 2.Descending.[descending means high to low / big to small]
+list=[56,3,7,27,19,5]
+# this is Ascending
+list.sort()
+print(list)
+# [3, 5, 7, 19, 27, 56]
+
+# this is desending
+list.sort(reverse=True)
+print(list)
+# [56, 27, 19, 7, 5, 3]
+
+# print(list.sort()) can not write like this.it print none. 
---
+++ practice/practice.py
@@ -0,0 +1,6 @@
+list=[4,3,2,1,7,6,5]
+list.sort()
+print(list)
+
+list.sort(reverse=True)
+print(list)
