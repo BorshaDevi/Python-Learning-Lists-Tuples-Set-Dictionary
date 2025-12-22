7dd36b0653ef4c2760339412c29f79b84c09848f
Borsha Devi <borshadevi27@gmail.com>
2025-12-22 10:45:11 +0600

    Now I learned about little bit tuple.

---
+++ Tuples/tuples.py
@@ -0,0 +1,28 @@
+# Tuples is a built-in data type that lets us create immutable sequences of values.
+
+# 1.A tuple is written using parentheses ().
+# 2.If you give a single value like (1), it is not a tuple; it is treated as an integer. 
+# To create a single-element tuple, you must add a comma, like(1,)
+
+tup=()
+print(type(tup))
+# 'tuple'
+
+tup1=(1)
+print(tup1)
+# 1
+print(type(tup1))
+# int
+
+tup2=(1,)
+print(tup2)
+# (1,)
+print(type(tup2))
+# tuple
+
+tup3=(1,2,3,4)
+print(tup3[0])
+# 1
+
+# tup1[0]=9
+# This operation is not allowed because tuples are immutable.
