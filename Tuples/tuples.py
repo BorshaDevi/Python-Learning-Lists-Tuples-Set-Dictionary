8164facd66771fbae882e643f5cf9b2843fb375b
Borsha Devi <borshadevi27@gmail.com>
2025-12-23 08:41:19 +0600

    Now I learned about tuple method index which return first index

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -1,36 +1,47 @@
-# Tuples is a built-in data type that lets us create immutable sequences of values.
+# # Tuples is a built-in data type that lets us create immutable sequences of values.
 
-# 1.A tuple is written using parentheses ().
-# 2.If you give a single value like (1), it is not a tuple; it is treated as an integer. 
-# To create a single-element tuple, you must add a comma, like(1,)
+# # 1.A tuple is written using parentheses ().
+# # 2.If you give a single value like (1), it is not a tuple; it is treated as an integer. 
+# # To create a single-element tuple, you must add a comma, like(1,)
 
-tup=()
-print(type(tup))
-# 'tuple'
+# tup=()
+# print(type(tup))
+# # 'tuple'
 
-tup1=(1)
-print(tup1)
+# tup1=(1)
+# print(tup1)
+# # 1
+# print(type(tup1))
+# # int
+
+# tup2=(1,)
+# print(tup2)
+# # (1,)
+# print(type(tup2))
+# # tuple
+
+# tup3=(1,2,3,4)
+# print(tup3[0])
+# # 1
+
+# # tup1[0]=9
+# # This operation is not allowed because tuples are immutable.
+
+# # Tuple slicing:
+# #  we can slice in tuple also.
+
+# tup4=(1,2,3,4,5,6,7)
+# print(tup4[1:3])
+# # (2, 3)
+
+# Tuple method
+
+# index():In this function,returns the first occurrence (first index) of the given value.
+
+tup5=(1,2,1,3,4,5,2,6,5)
+print(tup5.index(2))
 # 1
-print(type(tup1))
-# int
-
-tup2=(1,)
-print(tup2)
-# (1,)
-print(type(tup2))
-# tuple
-
-tup3=(1,2,3,4)
-print(tup3[0])
-# 1
-
-# tup1[0]=9
-# This operation is not allowed because tuples are immutable.
-
-# Tuple slicing:
-#  we can slice in tuple also.
-
-tup4=(1,2,3,4,5,6,7)
-print(tup4[1:3])
-# (2, 3)
-
+print(tup5.index(1))
+# 0
+print(tup5.index(5))
+# 5
--- practice/practice.py
+++ practice/practice.py
@@ -1,22 +1,22 @@
-# list=[4,3,2,1,7,6,5]
-# list.sort()
-# print(list)
+list=[4,3,2,1,7,6,5]
+list.sort()
+print(list)
 
-# list.sort(reverse=True)
-# print(list)
+list.sort(reverse=True)
+print(list)
 
-# list.append(8)
-# list.sort()
-# print(list)
+list.append(8)
+list.sort()
+print(list)
 
-# computer_product=['Keyboard',"Mouse",'Sound Box',"Camera","Table"]
-# computer_product.sort()
-# print(computer_product)
+computer_product=['Keyboard',"Mouse",'Sound Box',"Camera","Table"]
+computer_product.sort()
+print(computer_product)
 
-# list3=["banana",'apple','banana']
-# list3.remove('banana')
-# print(list3)
-# # ['apple', 'banana']
+list3=["banana",'apple','banana']
+list3.remove('banana')
+print(list3)
+# ['apple', 'banana']
 
 tuple=(1,2,3,4,5,6,7)
 print(tuple[3:5])
