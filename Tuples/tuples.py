38c581a2360b06023e55c270605af3563466c40d
Borsha Devi <borshadevi27@gmail.com>
2025-12-23 12:46:48 +0600

    practice 1,2,3,4 question in practice.py file

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -1,38 +1,38 @@
-# # Tuples is a built-in data type that lets us create immutable sequences of values.
+# Tuples is a built-in data type that lets us create immutable sequences of values.
 
-# # 1.A tuple is written using parentheses ().
-# # 2.If you give a single value like (1), it is not a tuple; it is treated as an integer. 
-# # To create a single-element tuple, you must add a comma, like(1,)
+# 1.A tuple is written using parentheses ().
+# 2.If you give a single value like (1), it is not a tuple; it is treated as an integer. 
+# To create a single-element tuple, you must add a comma, like(1,)
 
-# tup=()
-# print(type(tup))
-# # 'tuple'
+tup=()
+print(type(tup))
+# 'tuple'
 
-# tup1=(1)
-# print(tup1)
-# # 1
-# print(type(tup1))
-# # int
+tup1=(1)
+print(tup1)
+# 1
+print(type(tup1))
+# int
 
-# tup2=(1,)
-# print(tup2)
-# # (1,)
-# print(type(tup2))
-# # tuple
+tup2=(1,)
+print(tup2)
+# (1,)
+print(type(tup2))
+# tuple
 
-# tup3=(1,2,3,4)
-# print(tup3[0])
-# # 1
+tup3=(1,2,3,4)
+print(tup3[0])
+# 1
 
-# # tup1[0]=9
-# # This operation is not allowed because tuples are immutable.
+# tup1[0]=9
+# This operation is not allowed because tuples are immutable.
 
-# # Tuple slicing:
-# #  we can slice in tuple also.
+# Tuple slicing:
+#  we can slice in tuple also.
 
-# tup4=(1,2,3,4,5,6,7)
-# print(tup4[1:3])
-# # (2, 3)
+tup4=(1,2,3,4,5,6,7)
+print(tup4[1:3])
+# (2, 3)
 
 # Tuple method
 
